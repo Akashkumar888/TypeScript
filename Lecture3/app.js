60d07d785b9b599db8aefff366f78d9dbeeb594c
Akash kumar <akashgkr12@gmail.com>
2025-09-19 23:43:09 +0530

    custom types

--- Lecture3/app.js
+++ Lecture3/app.js
@@ -18,3 +18,34 @@ var concatOfTwoString = combine("Akash", "Kumar");
 if (typeof concatOfTwoString === "string") {
     console.log("Concatenation:", concatOfTwoString); // ✅ Safe
 }
+function akash(num1, num2, conversionType) {
+    var ans;
+    if (conversionType === 'as-number' || (typeof num1 === "number" && typeof num2 === "number")) {
+        ans = +num1 + +num2;
+    }
+    else {
+        ans = num1.toString() + num2.toString();
+    }
+    return ans;
+}
+var sum1 = akash(20, 30, "as-number");
+var sum2 = akash("50", "40", "as-number");
+var combineName = akash("Akash", "kumar", "as-string");
+console.log(sum1, sum2, combineName);
+// type → often used for aliases, unions, intersections.
+// interface → often used when designing shapes of objects, especially in OOP style or extending multiple interfaces.
+// interface ObjectType {
+//  name:string,
+//  age:number,
+//  skills:string[],
+// };
+var user = {
+    name: "Akash",
+    age: 22,
+    skills: ["Python", "cpp", "JavaScript", "Reactjs"],
+};
+console.log(user);
+function greet(user) {
+    console.log("Hii, I am ".concat(user.name));
+}
+greet(user);
--- Lecture3/app.ts
+++ Lecture3/app.ts
@@ -25,9 +25,9 @@ console.log("Hello...");
 
 // union -> pipe |
 // leteral types
-// type alias surtom types
+// type alias custom types
 
-// Union -> pipe |
+//! Union -> pipe |
 // Literal types
 // Type alias custom types
 
@@ -56,3 +56,89 @@ if (typeof concatOfTwoString === "string") {
 }
 
 
+
+//! Literal types | custom type use here 
+// function akash(num1: number|string, num2: number|string,conversionType:"as-number" | "as-string"){
+//   let ans;
+//   if(conversionType==='as-number' || (typeof num1 === "number" && typeof num2==="number")){
+//     ans= +num1 + +num2;
+//   }
+//   else{
+//     ans=num1.toString()+num2.toString();
+//   }
+//   return ans;
+// }
+
+// const sum1=akash(20,30,"as-number");
+// const sum2=akash("50","40","as-number");
+
+// const combineName=akash("Akash","kumar","as-string");
+
+// console.log(sum1,sum2,combineName);
+
+
+// types alias / custom types
+
+type combinable=number |string | boolean | string[];
+type ConversionType="as-number" | "as-string";
+function akash(num1: combinable, num2: combinable,conversionType:ConversionType){
+  let ans;
+  if(conversionType==='as-number' || (typeof num1 === "number" && typeof num2==="number")){
+    ans= +num1 + +num2;
+  }
+  else{
+    ans=num1.toString()+num2.toString();
+  }
+  return ans;
+}
+
+const sum1=akash(20,30,"as-number");
+const sum2=akash("50","40","as-number");
+
+const combineName=akash("Akash","kumar","as-string");
+
+console.log(sum1,sum2,combineName);
+
+
+
+
+// practice type in object
+// const user:{
+//   name:string,
+//   age:number,
+// }={
+//   name:"Akash",
+//   age:22,
+// }
+
+
+type ObjectType={
+ name:string,
+ age:number,
+ skills:string[],
+};
+
+// type → often used for aliases, unions, intersections.
+// interface → often used when designing shapes of objects, especially in OOP style or extending multiple interfaces.
+
+// interface ObjectType {
+//  name:string,
+//  age:number,
+//  skills:string[],
+// };
+
+
+const user : ObjectType ={
+  name:"Akash",
+  age:22,
+  skills:["Python","cpp","JavaScript","Reactjs"],
+}
+
+console.log(user);
+
+
+function greet(user:ObjectType){
+  console.log(`Hii, I am ${user.name}`);
+}
+
+greet(user);
