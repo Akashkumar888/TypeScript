4240ddd57fada7ab4f36166d4c5236cdf534d942
Akash kumar <akashgkr12@gmail.com>
2025-10-08 03:47:57 +0530

    classes

--- Lecture6/classes.js
+++ Lecture6/classes.js
@@ -1,8 +1,36 @@
 "use strict";
 console.log("This is classes");
-// tsc classes.ts --watch
-// tsc classes.ts -w
+// tsc classes.ts --watch   -> classes.ts ko watch karega 
+// tsc classes.ts -w         -> classes.ts ko watch karega 
 // both above same
-// tsc --init this makes tsconfig.json file 
-// tsc --watch
-// tsc -w 
+// tsc --init this create tsconfig.json file 
+// tsc --watch        -> ek folder ke sare file ko watch karega 
+// tsc -w             -> ek folder ke sare file ko watch karega
+// private public access modifiers
+class Department {
+    constructor(n) {
+        this.name = n;
+        this.employees = [];
+    }
+    describe(Department) {
+        console.log("Department", this.name);
+    }
+    addEmployee(emp) {
+        this.employees.push(emp);
+    }
+    printEmployee() {
+        console.log("Number of employee: ", this.employees.length);
+        console.log("Name of employee: ", this.employees);
+    }
+}
+;
+const accounting = new Department("CSE");
+accounting.describe();
+// const accountingCopy={
+//   name:"ECE",
+//   describe:accounting.describe
+// }
+// accountingCopy.describe();
+accounting.addEmployee("Akash");
+accounting.addEmployee("Priya");
+accounting.printEmployee();
--- Lecture6/interface.js
+++ Lecture6/interface.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log("This is interface");
+// tsc classes.ts --watch   -> classes.ts ko watch karega 
+// tsc classes.ts -w         -> classes.ts ko watch karega 
+// both above same
+// tsc --init this create tsconfig.json file 
+// tsc --watch        -> ek folder ke sare file ko watch karega 
+// tsc -w             -> ek folder ke sare file ko watch karega
